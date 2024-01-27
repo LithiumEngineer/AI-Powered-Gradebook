27e80e06ec8567346610c3236d1f7333224643ce
SuperMK15 <smartiskush@gmail.com>
2024-01-27 18:28:55 -0500

    updated delete button styling

--- frontend/src/components/Dashboard/Button.jsx
+++ frontend/src/components/Dashboard/Button.jsx
@@ -1,9 +1,23 @@
 import React from "react"
+import { RiDeleteBinLine } from 'react-icons/ri'
 
-const Button = ({ name, onClick, disabled=false }) => {
+const Button = ({ name, onClick, disabled = false }) => {
+  if (name === "Delete") {
+    return (
+      <div
+        onClick={onClick}
+        className="bg-[#F09C67] w-fit border-2 border-solid border-[#F09C67] px-4 py-[6px] rounded-full text-sm text-white mr-3 hover:cursor-pointer hover:bg-[#FFFDE8] hover:text-[#F09C67] hover:underline duration-100"
+      >
+        <div className="flex justify-center items-center">
+          <RiDeleteBinLine />
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div
-      onClick={disabled? null : onClick}
+      onClick={disabled ? null : onClick}
       className="bg-[#4C8492] w-fit border-2 border-solid border-[#4C8492] px-4 py-[6px] rounded-full text-sm text-white mr-3 hover:cursor-pointer hover:bg-[#FFFDE8] hover:text-[#4C8492] hover:underline duration-100"
     >
       {name}
