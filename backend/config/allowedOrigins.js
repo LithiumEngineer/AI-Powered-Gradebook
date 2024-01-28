122a025800ccab076da0bb63e4bb249d72aaeaad
SuperMK15 <smartiskush@gmail.com>
2024-01-28 01:39:19 -0500

    removed a bracket

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    process.env.NODE_ENV === 'production' ? 'https://worksheeps.onrender.com/' : 'http://localhost:3000'
+    process.env.NODE_ENV === 'production' ? 'https://worksheeps.onrender.com' : 'http://localhost:3000'
 ];
 
 module.exports = allowedOrigins;
