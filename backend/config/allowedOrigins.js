1a902f65ffb227fbbb3181ae9613b5f4c714c96c
SuperMK15 <smartiskush@gmail.com>
2024-01-28 02:16:37 -0500

    updated for new url

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    process.env.NODE_ENV === 'production' ? 'https://worksheeps.onrender.com' : 'http://localhost:3000'
+    process.env.NODE_ENV === 'production' ? 'https://worksheeps.onrender.com' : 'http://localhost:3000',
+    process.env.NODE_ENV === 'production' ? 'https://worksheeps.tech' : null
 ];
 
 module.exports = allowedOrigins;
